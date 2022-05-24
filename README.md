f835dd46589eeae31554a19eabad19960f55710d
Erik Waldrop <erik@davidsmightywarrior.com>
2022-05-24 17:49:42 +0000

    new script

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Python3Class
+Testing Version controlling
---
+++ helloworld.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+#print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+
+num = 32
+text = "Get a life"
+print(text, num)
+print(text, num, sep=' ', end='\n')
